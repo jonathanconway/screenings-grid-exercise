966e7a2ec0f203e73c5712bd65e9a737c281c8da
Jonathan Conway <jonathan.conway@gmail.com>
2019-09-03 02:57:25 -0700

    refactor: remove unnecessary code

--- src/components/filter-builder/FilterBuilder.tsx
+++ src/components/filter-builder/FilterBuilder.tsx
@@ -69,43 +69,39 @@ export const FilterBuilder = <T extends object = {}>(props: FilterBuilderProps<T
     <Container>
       {props.filters.map((filter, filterIndex) => {
         const field = fieldsMap[filter.columnKey] as Column<T>;
-        return <StyledFilter key={filterIndex}>
-          {true
-            ? <>
-                <FilterFieldSelect
-                  value={filter.columnKey.toString()}
-                  options={props.fields.map(f => ({ value: f.key.toString(), label: f.label }))}
-                  onChange={(value) => handleChangeFilterField(filter, value as keyof T)}
-                />
+        return (
+          <StyledFilter key={filterIndex}>
+            <FilterFieldSelect
+              value={filter.columnKey.toString()}
+              options={props.fields.map(f => ({ value: f.key.toString(), label: f.label }))}
+              onChange={(value) => handleChangeFilterField(filter, value as keyof T)}
+            />
 
-                <FilterOperatorSelect
-                  value={filter.operator}
-                  options={operators.map(o => ({ value: o, label: o }))}
-                  onChange={(value) => handleChangeFilterOperator(filter, value as Operator)}
-                />
+            <FilterOperatorSelect
+              value={filter.operator}
+              options={operators.map(o => ({ value: o, label: o }))}
+              onChange={(value) => handleChangeFilterOperator(filter, value as Operator)}
+            />
 
-                {!(field && field.options)
-                  ? <FilterValueInput
-                      value={filter.value as unknown as string}
-                      onInput={(value) => handleChangeFilterValue(filter, value)} />
-                  : <FilterValueSelect
-                      value={filter.value as unknown as string}
-                      options={[
-                        { value: "", label: "(any)" },
-                        ...(field.options.map(o => ({ value: o.value as unknown as string, label: o.label })))
-                      ]}
-                      onChange={(value) => handleChangeFilterValue(filter, value)} />}
-              </>
-            : <>
-                <FilterName>{filter.columnKey}</FilterName>
-                <FilterOperator>{filter.operator}</FilterOperator>
-                <FilterValue>{filter.value}</FilterValue>
-              </>
-          }
-          <DeleteFilterButton onClick={() => handleDeleteFilter(filter)}>
-            <IconButton icon="delete">Delete filter</IconButton>
-          </DeleteFilterButton>
-        </StyledFilter>;
+            {!(field && field.options)
+              ? <FilterValueInput
+                  value={filter.value as unknown as string}
+                  onInput={(value) => handleChangeFilterValue(filter, value)}
+                />
+              : <FilterValueSelect
+                  value={filter.value as unknown as string}
+                  options={[
+                    { value: "", label: "(any)" },
+                    ...(field.options.map(o => ({ value: o.value as unknown as string, label: o.label })))
+                  ]}
+                  onChange={(value) => handleChangeFilterValue(filter, value)}
+                />}
+
+            <DeleteFilterButton onClick={() => handleDeleteFilter(filter)}>
+              <IconButton icon="delete">Delete filter</IconButton>
+            </DeleteFilterButton>
+          </StyledFilter>
+        );
       })}
       <AddFilterButton onClick={handleAddFilter}>
         <IconButton icon="add">Add new filter</IconButton>
